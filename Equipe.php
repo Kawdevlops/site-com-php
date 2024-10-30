ec41ab297bc135038e74014d215c53c0d6412718
Kawdevlops <kauany.violin@gmail.com>
2024-10-30 14:24:56 -0300

    30/10/2024
    Criando paginas com html. porém dentro de php

---
+++ Equipe.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Equipe - Site </title>
+</head>
+<body>
+    <header>
+        <h1> Site exemplo</h1>
+        <nav>
+            <a href=""> Home </a>
+            <a href=""> Equipe </a>
+            <a href=""> Serviços </a>
+        </nav>
+    </header>
+
+    <main>
+        <article>
+            <h2> Nossa equipe! </h2>
+            <ul>
+                <li> Zury </li>
+                <li> Lexa </li>
+                <li> Mila </li>
+            </ul>
+            
+        </article>
+    </main>
+
+    <footer>
+        <h2> Site criado como exemplo </h2>
+    </footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Site </title>
+</head>
+<body>
+    <header>
+        <h1> Site exemplo</h1>
+        <nav>
+            <a href=""> Home </a>
+            <a href=""> Equipe </a>
+            <a href=""> Serviços </a>
+        </nav>
+    </header>
+
+    <main>
+        <article>
+            <h2> Bem vindoª!</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est veritatis voluptatibus assumenda.</p>
+        </article>
+    </main>
+
+    <footer>
+        <h2> Site criado como exemplo </h2>
+    </footer>
+
+</body>
+</html>
